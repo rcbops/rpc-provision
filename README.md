aae344ada49854f363f739c38e062e274a5fd01c
Evan Callicoat <apsu@propter.net>
2014-07-21 16:38:23 -0500

    Use dynamic inventory

---
+++ README.md
@@ -0,0 +1,27 @@
+#Usage
+
+Use this to provision cloud servers you've already configured.
+
+This expects a .config.yml in the clone root formatted as follows:
+
+```
+---
+# Rackspace OpenStack credentials
+env:
+  OS_USERNAME: "username"
+  OS_TENANT_ID: "12345"
+  OS_AUTH_SYSTEM: "rackspace"
+  OS_PASSWORD: "reallylongapikeyhere"
+  OS_AUTH_URL: "https://identity.api.rackspacecloud.com/v2.0/"
+  OS_REGION_NAME: "IAD"
+  OS_NO_CACHE: "1"
+  NOVA_RAX_AUTH: "1"
+# Prefix for nodes used by nova list --name
+host_prefix: "my-node"
+# Container network CIDR prefix/mask to reconfigure
+container_cidr_prefix: "172.16.0."
+container_cidr_mask: "24"
+# VXLAN tunnel CIDR prefix/mask to configure
+tunnel_cidr_prefix: "172.16.1."
+tunnel_cidr_mask: "24"
+```
---
+++ inventory/hosts.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, generators
+
+import json
+import os
+import re
+import shlex
+import subprocess
+import yaml
+
+def get_config(config_file):
+  # Load config file
+  with open(config_file, "r") as fp:
+    return yaml.load(fp)
+
+def get_hosts():
+  # Get config dict
+  config = get_config(".config.yml")
+
+  # Add openstack creds to environment
+  env = os.environ
+  env.update(config["env"])
+
+  # Get list of servers from nova by host prefix
+  output = subprocess.check_output(
+    shlex.split(
+      "nova list --name {} --fields name,networks".format(config["host_prefix"])
+    ), env=env
+  )
+
+  # Ignore first three and last output lines
+  lines = output.splitlines()[3:-2]
+
+  # Initialize dynamic inventory output
+  hosts = {"cluster": {"hosts": []}, "_meta": {"hostvars": {}}}
+
+  # For each line
+  for index, line in enumerate(lines):
+    # Split lines into sections by "|" chars
+    bits = re.findall("[|] ([^|]*) ", line)
+
+    # Pickout IPv4 public addresses
+    ips = re.findall("public=(?:(.*), )?(.*);", bits[2])[0]
+    ipv4 = ips[0] if ":" not in ips[0] else ips[1]
+
+    # Update hostvars metadata
+    hosts["_meta"]["hostvars"].update(
+      {
+        bits[1].strip():
+        {
+          "id": bits[0].strip(),
+          "ansible_ssh_host": ipv4.strip(),
+          "ansible_ssh_user": "root",
+          "container_ip": config["container_cidr_prefix"] +
+                          str(index) +
+                          config["container_cidr_mask"],
+          "tunnel_ip": config["tunnel_cidr_prefix"] +
+                          str(index) +
+                          config["tunnel_cidr_mask"]
+        }
+      }
+    )
+
+    # Append hostname to cluster group
+    hosts["cluster"]["hosts"].append(bits[1].strip())
+
+  return hosts
+
+if __name__ == "__main__":
+  print(json.dumps(get_hosts(), indent=2, sort_keys=True))

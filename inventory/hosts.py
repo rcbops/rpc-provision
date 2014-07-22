2ac0eb30fb64673122b0632a9af08cb3523666a0
Evan Callicoat <apsu@propter.net>
2014-07-22 11:27:49 -0500

    Acquire the approval of flake8

--- inventory/hosts.py
+++ inventory/hosts.py
@@ -9,63 +9,70 @@ import shlex
 import subprocess
 import yaml
 
+
 def get_config(config_file):
-  # Load config file
-  with open(config_file, "r") as fp:
-    return yaml.load(fp)
+    # Load config file
+    with open(config_file, "r") as fp:
+        return yaml.load(fp)
+
 
 def get_hosts():
-  # Get config dict
-  config = get_config(".config.yml")
+    # Get config dict
+    config = get_config(".config.yml")
 
-  # Add openstack creds to environment
-  env = os.environ
-  env.update(config["env"])
+    # Add openstack creds to environment
+    env = os.environ
+    env.update(config["env"])
 
-  # Get list of servers from nova by host prefix
-  output = subprocess.check_output(
-    shlex.split(
-      "nova list --name {} --fields name,networks".format(config["host_prefix"])
-    ), env=env
-  )
-
-  # Ignore first three and last output lines
-  lines = output.splitlines()[3:-1]
-
-  # Initialize dynamic inventory output
-  hosts = {"cluster": {"hosts": []}, "_meta": {"hostvars": {}}}
-
-  # For each line
-  for index, line in enumerate(lines):
-    # Split lines into sections by "|" chars
-    bits = re.findall("[|] ([^|]*) ", line)
-
-    # Pickout IPv4 public addresses
-    ips = re.findall("public=(?:(.*), )?(.*);", bits[2])[0]
-    ipv4 = ips[0] if ":" not in ips[0] else ips[1]
-
-    # Update hostvars metadata
-    hosts["_meta"]["hostvars"].update(
-      {
-        bits[1].strip():
-        {
-          "id": bits[0].strip(),
-          "ansible_ssh_host": ipv4.strip(),
-          "ansible_ssh_user": "root",
-          "container_ip": config["container_cidr_prefix"] +
-                          str(index + 1) + "/" +
-                          config["container_cidr_mask"],
-          "tunnel_ip": config["tunnel_cidr_prefix"] +
-                          str(index + 1) + "/" +
-                          config["tunnel_cidr_mask"]
-        }
-      }
+    # Get list of servers from nova by host prefix
+    output = subprocess.check_output(
+        shlex.split(
+            "nova list --name {} --fields name,networks".format(
+                config["host_prefix"]
+            )
+        ),
+        env=env
     )
 
-    # Append hostname to cluster group
-    hosts["cluster"]["hosts"].append(bits[1].strip())
+    # Ignore first three and last output lines
+    lines = output.splitlines()[3:-1]
 
-  return hosts
+    # Initialize dynamic inventory output
+    hosts = {"cluster": {"hosts": []}, "_meta": {"hostvars": {}}}
+
+    # For each line
+    for index, line in enumerate(lines):
+        # Split lines into sections by "|" chars
+        bits = re.findall("[|] ([^|]*) ", line)
+
+        # Pickout IPv4 public addresses
+        ips = re.findall("public=(?:(.*), )?(.*);", bits[2])[0]
+        ipv4 = ips[0] if ":" not in ips[0] else ips[1]
+
+        # Update hostvars metadata
+        hosts["_meta"]["hostvars"].update(
+            {
+                bits[1].strip():
+                {
+                    "id": bits[0].strip(),
+                    "ansible_ssh_host": ipv4.strip(),
+                    "ansible_ssh_user": "root",
+                    "container_ip":
+                        config["container_cidr_prefix"] +
+                        str(index + 1) + "/" +
+                        config["container_cidr_mask"],
+                    "tunnel_ip":
+                        config["tunnel_cidr_prefix"] +
+                        str(index + 1) + "/" +
+                        config["tunnel_cidr_mask"]
+                }
+            }
+        )
+
+        # Append hostname to cluster group
+        hosts["cluster"]["hosts"].append(bits[1].strip())
+
+    return hosts
 
 if __name__ == "__main__":
-  print(json.dumps(get_hosts(), indent=2, sort_keys=True))
+    print(json.dumps(get_hosts(), indent=2, sort_keys=True))
